2822623b9933e47c235f483c971cf50c4eb250e7
liushangfeng <lsflsf520@126.com>
2018-03-15 21:20:20 +0800

    审核状态枚举

--- common-tools/src/main/java/com/xyz/tools/common/constant/CheckState.java
+++ common-tools/src/main/java/com/xyz/tools/common/constant/CheckState.java
@@ -4,9 +4,12 @@ public enum CheckState {
 
 	DELETED("已删除"),
 	CLOSED("已关闭"),
+	OFFLINE("已下架"),
+	EXPIRED("已失效"),
 	REJECT("审核失败"),
 	CHECKING("待审核"),
 	PASSED("已审核"),
+	ONLINE("已上架"),
 	;
 	
 	private String descp;
